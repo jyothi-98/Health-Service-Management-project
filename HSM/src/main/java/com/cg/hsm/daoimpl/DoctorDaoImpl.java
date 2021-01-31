f8ddf1d586dae42323549ed0dcbcbb60525a1df5
jyothi-98 <76990417+jyothi-98@users.noreply.github.com>
2021-01-31 14:27:58 +0530

    Update DoctorDaoImpl.java

--- HSM/src/main/java/com/cg/hsm/daoimpl/DoctorDaoImpl.java
+++ HSM/src/main/java/com/cg/hsm/daoimpl/DoctorDaoImpl.java
@@ -2,10 +2,17 @@ package com.cg.hsm.daoimpl;
 
 import java.util.List;
 
+import javax.persistence.Query;
+
 import com.cg.hsm.dao.DoctorDao;
 import com.cg.hsm.domain.Doctor;
 import com.cg.hsm.util.DbUtil;
 
+/**
+ * This class implements classes of DoctorDao Class
+ * @author Pranjali Chaudhari
+ *
+ */
 public class DoctorDaoImpl extends DbUtil implements DoctorDao {
 	
 	/**
@@ -23,8 +30,14 @@ public class DoctorDaoImpl extends DbUtil implements DoctorDao {
 	 * This updateDoctor method will update doctor fees in doctors table in database 
 	 */
 	@Override
-	public void updateDoctor(int fees) {
-		// TODO Auto-generated method stub
+	public void updateDoctorFee(int doctorId,int updatedDoctorFee) {
+		entityManager.getTransaction().begin();
+		
+		Doctor doctor = entityManager.find(Doctor.class, doctorId);
+		doctor.setDoctorFee(updatedDoctorFee);
+		
+		entityManager.getTransaction().commit();
+		entityManager.close();
 		
 	}
 
@@ -32,18 +45,36 @@ public class DoctorDaoImpl extends DbUtil implements DoctorDao {
 	 * This updateDoctor method will update doctor details in doctors table in database
 	 */
 	@Override
-	public void updateDoctor(Doctor doctor) {
-		// TODO Auto-generated method stub
+	public void updateDoctor(Doctor doctor1,int doctorId) {
+		entityManager.getTransaction().begin();
+		
+		Doctor doctor = entityManager.find(Doctor.class, doctorId);
+		doctor.setDoctorName(doctor1.getDoctorName());
+		doctor.setContactNumber(doctor1.getContactNumber());
+		doctor.setSpecialization(doctor1.getSpecialization());
+		doctor.setHoursOfAvailability(doctor1.getHoursOfAvailability());
+		doctor.setDegree(doctor1.getDegree());
+		doctor.setYearsOfExperience(doctor1.getYearsOfExperience());
+		doctor.setDoctorFee(doctor1.getDoctorFee());
+		
+		entityManager.getTransaction().commit();
+		entityManager.close();
+  
 		
 	}
 
 	/**
-	 * This deleteDcotor method will delete doctor details from doctors table in database
+	 * This deleteDoctor method will delete doctor details from doctors table in database
 	 */
 	@Override
-	public void deleteDoctor(Doctor doctor) {
-		// TODO Auto-generated method stub
+	public void deleteDoctor(int doctorId) {
+		entityManager.getTransaction().begin();
 		
+		Doctor doctor = entityManager.find(Doctor.class, doctorId);
+		entityManager.remove(doctor);
+		
+		entityManager.getTransaction().commit();
+		entityManager.close();
 	}
 
 	/**
@@ -51,7 +82,20 @@ public class DoctorDaoImpl extends DbUtil implements DoctorDao {
 	 */
 	@Override
 	public List<Doctor> findAll() {
-		// TODO Auto-generated method stub
+		Query query =entityManager.createQuery("from Doctor");	
+		List<Doctor> doctors =  query.getResultList();
+		for(Doctor doctor:doctors)
+		{
+			System.out.println("ID : " + doctor.getDoctorId());
+			System.out.println("Name : " + doctor.getDoctorName());
+			System.out.println("Contact Number : " + doctor.getContactNumber());
+			System.out.println("Specialization : " + doctor.getSpecialization());
+			System.out.println("Hours of Availability : " + doctor.getHoursOfAvailability());
+			System.out.println("Years of Experience : " + doctor.getYearsOfExperience());
+			System.out.println("Degree : " + doctor.getDegree());
+			System.out.println("Fees : " + doctor.getDoctorFee());
+			System.out.println("-----------------------------");
+		}
 		return null;
 	}
 
