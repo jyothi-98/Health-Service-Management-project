b6f5e45d117f19d6e78ba8193068f944a380ba77
jyothi-98 <76990417+jyothi-98@users.noreply.github.com>
2021-01-31 14:28:41 +0530

    Update PatientDaoImplTest.java

--- HSM/src/main/java/com/cg/hsm/test/PatientDaoImplTest.java
+++ HSM/src/main/java/com/cg/hsm/test/PatientDaoImplTest.java
@@ -13,7 +13,10 @@ import com.cg.hsm.domain.Patient;
 
 
 public class PatientDaoImplTest {
-	public static void main(String args[]) {
+	
+
+	private static void RegisterPatient() {
+		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		System.out.println("-------Please Provide below details---------");
 		Patient patient = new Patient();
@@ -29,11 +32,96 @@ public class PatientDaoImplTest {
 		patient.setSymptoms(sc.next());
 		System.out.println("Enter Patients's Admission Fees: ");
 		patient.setAdmissionFee(sc.nextInt());
+		System.out.println("Enter Patient's Policy Id : ");
+		patient.setPolicyId(sc.nextInt());
+		sc.nextLine();
+		System.out.println("Enter Patient's Policy Name : ");
+		patient.setPolicyName(sc.nextLine());
 		
 		PatientDaoImpl impl=new PatientDaoImpl();
 		impl.addPatient(patient);
 		System.out.println("Patient Registered Successfully!");
 		
 	}
+	
+	
+	
+	public static void UpdatePatient() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("------------Enter the data to be updated------------------");
+		Patient patient1=new Patient();
+		System.out.println("enter the id of the patient");
+		int patientId=sc.nextInt();
+		sc.nextLine();
+		System.out.println("enter the name ");
+		patient1.setPatientName(sc.next());
+		System.out.println("Enter Patient's Age : ");
+		patient1.setPatientAge(sc.nextInt());
+		System.out.println("Enter Patient's Contact Number : ");
+		patient1.setPatientContact(sc.nextLong());
+		System.out.println("Enter Patient's Address : ");
+		patient1.setAddress(sc.next());
+		System.out.println("Enter Patient's Symptoms: ");
+		patient1.setSymptoms(sc.next());
+		System.out.println("Enter Patients's Admission Fees: ");
+		patient1.setAdmissionFee(sc.nextInt());
+		System.out.println("Enter Patient's Policy Id : ");
+		patient1.setPolicyId(sc.nextInt());
+		sc.nextLine();
+		System.out.println("Enter Patient's Policy Name : ");
+		patient1.setPolicyName(sc.nextLine());
+		
+		PatientDaoImpl impl=new PatientDaoImpl();
+		impl.updatePatient(patient1,patientId);
+		System.out.println("Patient updated Successfully!");
+		
+		
+	}
+	private static void RemovePatient() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("-------Deleting details-------- ");
+		Patient patient = new Patient();
+		System.out.println("Enter Patient id : ");
+		int patientId = sc.nextInt();
+		PatientDaoImpl impl=new PatientDaoImpl();
+		
+		impl.deletePatient(patientId);
+		System.out.println("-------Deleted patient Details-------- ");
+		
+		
+	}
+		public static void main(String[] args) {
+			Scanner sc=new Scanner(System.in);
+			int continueChoice;
+			System.out.println("------MENU------");
+			System.out.println("1. Register patient");
+			System.out.println("2. FindAll ");
+			System.out.println("3. UpdatePatient");
+			System.out.println("4. RemovePatient");
+			System.out.println("Enter the choice");
+			int choice=sc.nextInt();
+			
+			switch (choice) {
+			case 1:
+					RegisterPatient();
+					break;
+			case 2:
+				PatientDaoImpl impl = new PatientDaoImpl();
+				impl.findAll();
+				System.out.println("-------------------");
+				break;
+			
+			case 3:
+					UpdatePatient();
+					break;
+			
+			case 4:
+					RemovePatient();
+					break;
+			default:
+				break;
+			}
+	
+		}
 
 }
